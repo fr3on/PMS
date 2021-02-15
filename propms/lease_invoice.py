7367289be378133ecad6bc58293182143eee9512
fr3on <hello@fr3on.info>
2021-02-16 01:53:12 +0200

    Update lease_invoice.py

--- propms/lease_invoice.py
+++ propms/lease_invoice.py
@@ -36,7 +36,7 @@ def makeInvoice(date,customer,items,currency=None,lease=None,lease_item=None,qty
 			posting_date=today(),
 			items=json.loads(items),
 			customer=str(customer),
-			due_date=subs_end_date,
+			due_date=schedule_start_date,
 			currency=currency,
 			lease=lease,
 			lease_item=lease_item,
